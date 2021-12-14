5541320d6091afa4a7ea1b18007ce02d09e8a4a4
Timur Turbil <timur.turbil@mlpcare.com>
2021-12-14 15:27:10 +0300

    first commit

---
+++ Background_Timer/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Background_Timer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Background_Timer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        readonly IHost host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Timer>();
+            })
+            .Build();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task StartTimer()
+        {
+            Console.WriteLine("StartTimer");
+            await host.RunAsync();
+            //await host.StartAsync();
+        }
+        [HttpPost]
+        public async Task StopTimer1()
+        {
+            Console.WriteLine("StopTimer1");
+            await host.StopAsync();
+            //await host.StopAsync(cancellationToken: CancellationToken.None);
+            //await host.WaitForShutdownAsync();
+            //host.Dispose();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Background_Timer/Timer.cs
@@ -0,0 +1,31 @@
+﻿namespace Background_Timer
+{
+    public class Timer : BackgroundService
+    {
+        private readonly ILogger<Timer> _logger;
+
+        public Timer(ILogger<Timer> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+           
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Timer running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+                //var isContinue = CheckIfContinue();
+                //if (isContinue){
+                //    _logger.LogInformation("Timer running at: {time}", DateTimeOffset.Now);
+                //    await Task.Delay(1000, stoppingToken);
+                //    continue;
+                //      }
+                //else{
+                //    break;
+                //  }
+            }
+        }
+    }
+}
