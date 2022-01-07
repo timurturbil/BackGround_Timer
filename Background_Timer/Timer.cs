0c45352bb52b8df991fcb65d7f810bebc7e5dee1
timurturbil <timurturbill@gmail.com>
2022-01-07 14:39:46 +0300

    update

--- Background_Timer/Timer.cs
+++ Background_Timer/Timer.cs
@@ -3,29 +3,51 @@
     public class Timer : BackgroundService
     {
         private readonly ILogger<Timer> _logger;
+        private Task _executingTask;
+		private CancellationTokenSource _cts;
 
         public Timer(ILogger<Timer> logger)
         {
             _logger = logger;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-           
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Timer running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-                //var isContinue = CheckIfContinue();
-                //if (isContinue){
-                //    _logger.LogInformation("Timer running at: {time}", DateTimeOffset.Now);
-                //    await Task.Delay(1000, stoppingToken);
-                //    continue;
-                //      }
-                //else{
-                //    break;
-                //  }
-            }
-        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+		{
+			_logger.LogWarning("Worker service started.");
+
+			_cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+			_executingTask = ExecuteAsync(_cts.Token);
+
+			return _executingTask.IsCompleted ? _executingTask : Task.CompletedTask;
+		}
+
+		public override Task StopAsync(CancellationToken cancellationToken)
+		{
+			if (_executingTask == null)
+			{
+				return Task.CompletedTask;
+			}
+
+			_logger.LogWarning("Worker service stopping.");
+
+			_cts.Cancel();
+
+			Task.WhenAny(_executingTask, Task.Delay(-1, cancellationToken)).ConfigureAwait(true);
+
+			cancellationToken.ThrowIfCancellationRequested();
+
+			_logger.LogWarning("Worker service stopped.");
+
+			return Task.CompletedTask;
+		}
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			while (!stoppingToken.IsCancellationRequested)
+			{
+				_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+				await Task.Delay(1000, stoppingToken);
+			}
+		}
     }
 }
